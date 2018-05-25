086ca5e879ad1cb85c77a65a72e4070ebd2295b8
Ryan Borja <ryan.t.bortja@gmail.com>
2018-05-25 14:26:58 -0400

    changed express code

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-/*
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+//
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
@@ -19,4 +17,3 @@ app.use(express.static(__dirname + '/'));
 app.listen(port, function() {
     console.log('I am listening on port ' + port +'!');
 });
-*/
