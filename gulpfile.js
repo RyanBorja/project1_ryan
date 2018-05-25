96dc6020f978e3c4aef6e5a55986328ba90fe115
Ryan Borja <ryan.t.bortja@gmail.com>
2018-05-25 11:08:07 -0400

    changed index.js and gulpfile because of GET error in heroku

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var inject = require('gulp-inject');
-var runSequence = require('run-sequence');
+//var inject = require('gulp-inject');
+//var runSequence = require('run-sequence');
 
-// gulp.task('copyhtml', function() {
-// 	return gulp.src('app/**/*.+(htm|html)')
-// 		.pipe(gulp.dest('dist/'))
-// });
+gulp.task('copyhtml', function() {
+	return gulp.src('app/**/*.+(htm|html)')
+		.pipe(gulp.dest('dist/'))
+});
 
 gulp.task('copycss', function() {
 	return gulp.src('app/css/**/*.css')
@@ -19,13 +19,13 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest('dist/css/'))
 });
 
-gulp.task('inject', function() {
-	var targets = gulp.src('app/**/*.+(htm|html)');
-	var sources = gulp.src(['dist/css/**/*.css'], {read: false});
-	return targets.pipe(inject(sources))
-		.pipe(gulp.dest('dist/'));
-});
-
-gulp.task('default', function(callback) {
-	runSequence(['sass', 'copycss'], 'inject', callback)
-});
+// gulp.task('inject', function() {
+// 	var targets = gulp.src('app/**/*.+(htm|html)');
+// 	var sources = gulp.src(['dist/css/**/*.css'], {read: false});
+// 	return targets.pipe(inject(sources))
+// 		.pipe(gulp.dest('dist/'));
+// });
+//
+// gulp.task('default', function(callback) {
+// 	runSequence(['sass', 'copycss'], 'inject', callback)
+// });
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*const express = require('express')
+const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -8,8 +8,8 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-*/
 
+/*
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
@@ -19,3 +19,4 @@ app.use(express.static(__dirname + '/'));
 app.listen(port, function() {
     console.log('I am listening on port ' + port +'!');
 });
+*/
